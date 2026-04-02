74b797014f523728834ab4dada741efc840b0deb
AjayPaul7188 <ajaypaulmummdi@gmail.com>
2026-04-02 22:57:37 +0530

    Database connected

---
+++ server/src/server.ts
@@ -0,0 +1,30 @@
+import app from "./app";
+import http from "http";
+import { Server } from "socket.io";
+import { connectDB } from "./config/db";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+connectDB();
