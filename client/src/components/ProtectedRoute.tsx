438789c7ca879c63ae56ce4ae9961f2d11074475
AjayPaul7188 <ajaypaulmummdi@gmail.com>
2026-05-07 14:15:24 +0530

    add production api urls

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
-  const token = sessionStorage.getItem("token"); // ✅ FIX
+  const token = sessionStorage.getItem("token"); 
 
   if (!token) {
     return <Navigate to="/auth" replace />;
